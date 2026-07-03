f1f2d003eed6317b3e5e07d290c53240c1e0bbd4
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2026-07-03 14:51:34 +0530

    Day 1: Initial Spring Boot project setup

---
+++ src/main/java/com/abhyasika/AbhyasikaManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.abhyasika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbhyasikaManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbhyasikaManagementSystemApplication.class, args);
+	}
+
+}
